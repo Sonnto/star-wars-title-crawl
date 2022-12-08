453f5015d7fdab94e462accbc4eaa2e1eb7f172b
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-12-08 11:31:00 -0500

    refactor code

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,21 @@
-const stars = 400;
+const starAmount = 800;
 
-for (let i = 0; i < stars; i++) {
+for (let i = 0; i < starAmount; i++) {
   let star = document.createElement("div");
+
   star.className = "stars";
+
   let xy = randomize();
+
   star.style.top = xy[0] + "px";
   star.style.left = xy[1] + "px";
+
   document.body.append(star);
 }
 
 function randomize() {
-  let y = window.innerWidth;
   let x = window.innerHeight;
+  let y = window.innerWidth;
   let randomX = Math.floor(Math.random() * x);
   let randomY = Math.floor(Math.random() * y);
   return [randomX, randomY];
