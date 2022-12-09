5ec08ba171970acc94800c6cf5cfebadbaff4d86
Sonnto <90372802+Sonnto@users.noreply.github.com>
2022-12-09 15:12:07 -0500

    fix addEventListener typo

--- javascript/script.js
+++ javascript/script.js
@@ -29,35 +29,60 @@ function randomize() {
 
 let btnElement = document.getElementById("btn");
 
-setTimeout(function () {
-  document.getElementById("background-music").play();
-  console.log("Audio played now, when the Star Wars logo flies in");
-}, 8300);
-
 btnElement.addEventListener("click", function () {
-  let introElement = document.getElementsByClassName("intro");
-  let logoElement = document.getElementsByClassName("logo");
-  let crawlTextElement = document.getElementsByClassName("crawl-text");
-  if (
-    introElement.style.animationPlayState === "running" &&
-    introElement.style.display === "block" &&
-    logoElement.style.animationPlayState === "running" &&
-    logoElement.style.display === "block" &&
-    crawlTextElement.style.animationPlayState === "running" &&
-    crawlTextElement.style.display === "block"
-  ) {
-    introElement.style.animationPlayState = "paused";
-    introElement.style.display = "none";
-    logoElement.style.animationPlayState = "paused";
-    logoElement.style.display = "none";
-    crawlTextElement.style.animationPlayState = "paused";
-    crawlTextElement.style.display = "none";
-  } else {
-    introElement.style.animationPlayState = "running";
-    introElement.style.display = "block";
-    logoElement.style.animationPlayState = "running";
-    logoElement.style.display = "block";
-    crawlTextElement.style.animationPlayState = "running";
-    crawlTextElement.style.display = "block";
-  }
+  setTimeout(function () {
+    document.getElementById("background-music").play();
+    console.log("Audio played now, when the Star Wars logo flies in");
+  }, 8600);
+  btnElement.classList.toggle("hideButton");
+  playAll();
+  // console.log("test");
 });
+
+function playAll() {
+  let introElement = document.getElementsByClassName("intro");
+  introElement.classList.toggle("playShow");
+
+  let logoElement = document.getElementsByClassName("logo");
+  logoElement.classList.toggle("playShow");
+
+  let crawlTextElement = document.getElementsByClassName("crawl-text");
+  crawlTextElement.classList.toggle("playShow");
+}
+
+// playAll();
+
+// btnElement.addEventListener("click", function () {
+//   let introElement = document.getElementsByClassName("intro");
+//   let logoElement = document.getElementsByClassName("logo");
+//   let crawlTextElement = document.getElementsByClassName("crawl-text");
+
+//   function playAll() {
+//     introElement.classList.toggle("playShow");
+//     logoElement.classList.toggle("playShow");
+//     crawlTextElement.classList.toggle("playShow");
+//   }
+
+//   if (
+//     introElement.style.animationPlayState === "running" &&
+//     introElement.style.display === "block" &&
+//     logoElement.style.animationPlayState === "running" &&
+//     logoElement.style.display === "block" &&
+//     crawlTextElement.style.animationPlayState === "running" &&
+//     crawlTextElement.style.display === "block"
+//   ) {
+//     introElement.style.animationPlayState = "paused";
+//     introElement.style.display = "none";
+//     logoElement.style.animationPlayState = "paused";
+//     logoElement.style.display = "none";
+//     crawlTextElement.style.animationPlayState = "paused";
+//     crawlTextElement.style.display = "none";
+//   } else {
+//     introElement.style.animationPlayState = "running";
+//     introElement.style.display = "block";
+//     logoElement.style.animationPlayState = "running";
+//     logoElement.style.display = "block";
+//     crawlTextElement.style.animationPlayState = "running";
+//     crawlTextElement.style.display = "block";
+//   }
+// });
